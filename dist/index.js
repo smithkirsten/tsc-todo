5e197b3040db68c8b52e597a05d09ae167bd7070
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-03-16 17:30:29 -0400

    Add event listener and action

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log('hello');
+const form = document.querySelector('form');
+const button = document.getElementById('clickButton');
+const input = document.getElementById('toDoInput');
+const list = document.getElementById('toDoList');
+const handleSubmit = (e) => {
+    e.preventDefault();
+    const newToDo = input.value;
+    const newLI = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    newLI.append(newToDo);
+    newLI.append(checkbox);
+    list.append(newLI);
+    input.value = '';
+};
+form.addEventListener('submit', handleSubmit);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('hello')
+const form = document.querySelector('form')!;
+const button = document.getElementById('clickButton')! as HTMLButtonElement;
+const input = document.getElementById('toDoInput')! as HTMLInputElement;
+const list = document.getElementById('toDoList')!
+
+
+const handleSubmit = (e: SubmitEvent) => {
+  e.preventDefault();
+  const newToDo = input.value
+  list.innerHTML += `<li>${newToDo}</li>`
+  input.value=''
+};
+
+
+form.addEventListener('submit', handleSubmit)
