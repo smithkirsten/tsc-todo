f6c3f1b132258d97c8bd05d20677f7c62cfd1007
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-03-17 09:34:08 -0400

    Add interface and break up event listener

--- dist/index.js
+++ dist/index.js
@@ -3,15 +3,21 @@ const form = document.querySelector('form');
 const button = document.getElementById('clickButton');
 const input = document.getElementById('toDoInput');
 const list = document.getElementById('toDoList');
+const todos = [];
+const displayList = () => {
+    list.innerHTML = todos.map(todo => `<li><input type="checkbox" checked=${todo.completed}/>${todo.text}</li>`).join('');
+};
+const addToDo = () => {
+    const newToDo = {
+        text: input.value,
+        completed: false
+    };
+    todos.push(newToDo);
+    input.value = '';
+};
 const handleSubmit = (e) => {
     e.preventDefault();
-    const newToDo = input.value;
-    const newLI = document.createElement('li');
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    newLI.append(newToDo);
-    newLI.append(checkbox);
-    list.append(newLI);
-    input.value = '';
+    addToDo();
+    displayList();
 };
 form.addEventListener('submit', handleSubmit);
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,32 @@
+interface Todo {
+  text: string;
+  completed: boolean;
+}
+
 const form = document.querySelector('form')!;
 const button = document.getElementById('clickButton')! as HTMLButtonElement;
 const input = document.getElementById('toDoInput')! as HTMLInputElement;
 const list = document.getElementById('toDoList')!
 
+const todos: Todo[] = [];
+
+const displayList = () => {
+  list.innerHTML = todos.map(todo => `<li><input type="checkbox" checked=${todo.completed}/>${todo.text}</li>`).join('')
+}
+
+const addToDo = () => {
+  const newToDo: Todo = {
+    text: input.value,
+    completed: false
+  }
+  todos.push(newToDo)
+  input.value=''
+}
 
 const handleSubmit = (e: SubmitEvent) => {
   e.preventDefault();
-  const newToDo = input.value
-  list.innerHTML += `<li>${newToDo}</li>`
-  input.value=''
+  addToDo();
+  displayList();
 };
 
 
